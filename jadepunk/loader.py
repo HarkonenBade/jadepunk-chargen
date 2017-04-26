242e6ad4337aa702be4370c4c91e90ebd6217648
Thomas Bytheway <github@harkonen.net>
2017-04-26 23:00:27 +0100

    Smarter yaml parsing and support for Talented

--- jadepunk/loader.py
+++ jadepunk/loader.py
@@ -6,6 +6,33 @@ from . import Aspects, AspectTypes, Asset, AssetTypes, Attrs, AttrTypes, Charact
 from .engine import EngineLoader
 
 
+def get_prop_class(name):
+    return getattr(Asset, name)
+
+
+def props(args):
+    return {k: v for k, v in args.items() if k[0].isupper()}
+
+
+def non_props(args):
+    return {k: v for k, v in args.items() if not k[0].isupper()}
+
+
+def make_prop(name, args):
+    cls = get_prop_class(name)
+    if cls == Asset.Talented:
+        prop = None
+        for key in props(args):
+            prop = make_prop(key, args[key])
+        return cls(prop=prop, **non_props(args))
+    if isinstance(args, dict):
+        return cls(**args)
+    elif isinstance(args, list):
+        return cls(*args)
+    else:
+        return cls(args)
+
+
 def from_yaml(yml_path, engine):
     def load_type(enum, tag):
         def load(loader, node):
@@ -24,21 +51,15 @@ def from_yaml(yml_path, engine):
     for asset in char_data['assets']:
         features = []
         flaws = []
-        for key in asset:
-            if key[0].isupper():
-                cls = getattr(Asset, key)
-                if isinstance(asset[key], dict):
-                    obj = cls(**asset[key])
-                else:
-                    obj = cls(asset[key])
-                if issubclass(cls, Asset.Feature):
-                    features.append(obj)
-                else:
-                    flaws.append(obj)
-        params = {k: v for k, v in asset.items() if not k[0].isupper()}
+        for key in props(asset):
+            prop = make_prop(key, asset[key])
+            if isinstance(prop, Asset.Feature):
+                features.append(prop)
+            else:
+                flaws.append(prop)
         char_assets.append(Asset(features=features,
                                  flaws=flaws,
-                                 **params))
+                                 **non_props(asset)))
     char_params = {k: v for k, v in char_data.items() if k not in ['aspects', 'attrs', 'assets']}
     Character(aspects=char_aspects,
               attrs=char_attrs,
